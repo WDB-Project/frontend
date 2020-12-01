b76252a0f91e9cd326d520d502ca5c102db674f1
Kimberley Fan <kimberley.fan123@berkeley.edu>
2020-12-01 00:58:07 -0800

    merged navbar sign in and browse

--- final-proj/src/components/Event_Card/Event_Card.js
+++ final-proj/src/components/Event_Card/Event_Card.js
@@ -1,1 +1,26 @@
-<h>hello world;</h>;
+import React, { Component } from "react";
+import "./event_card.css";
+
+const Event_Card = ({ event }) => {
+  return (
+    <div class="entire-card">
+      <img src="http://placecorgi.com/310/178.34" alt="Pictures of Events" />
+      <div class="card-interior">
+        <h5 class="title-card">{event.title}</h5>
+        <div class="date-of-event">
+          <div class="location-event">{event.location}</div>
+        </div>
+        <div class="date-of-event">{event.date}</div>
+
+        <ul class="tags">
+          <li>
+            <a href="#" class="tag">
+              {event.tag}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Event_Card;
