e703d5621510174943f78be7a9d76f94ff5639bf
Kimberley Fan <kimberley.fan123@berkeley.edu>
2020-11-30 22:45:16 -0800

    browse page updated filter and cards

--- final-proj/src/App.js
+++ final-proj/src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import EventPage from  './components/EventPage/EventPage'
-import Header from './components/NavBar/NavBar'
-import SignIn from './components/SignIn/SignIn'
+import logo from "./logo.svg";
+import "./App.css";
+import EventPage from "./components/EventPage/EventPage";
+import Header from "./components/NavBar/NavBar";
+import SignIn from "./components/SignIn/SignIn";
+import React, { Component } from "react";
+import Browse_Content from "./components/Browse_Content/browse_content";
 
 function App() {
   return (
-    <SignIn/>
-      // <EventPage eventID = "test_volunteer_event" />
+    <div>
+      <Browse_Content />
+      <SignIn />
+    </div>
+    // <EventPage eventID = "test_volunteer_event" />
   );
 }
 
---
+++ final-proj/src/components/Browse_Content/browse_content.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import "./browse_content.css";
+import Event_Card from "../Event_Card/Event_Card";
+import Filter from "../filter/filter";
+
+const a = [
+  {
+    title: "Protest",
+    location: "Walnut, CA",
+    date: "March 23, 2018",
+    time: "3:30PM",
+    tag: "Social Good",
+  },
+  {
+    title: "Birthday Party",
+    location: "Alaska, CA",
+    date: "May 29, 2020",
+    time: "2:30PM",
+    tag: "Music",
+  },
+  {
+    title: "Career Fair",
+    location: "Berkeley, CA",
+    date: "August 23, 2018",
+    time: "3:30PM",
+    tag: "Professional",
+  },
+  {
+    title: "Protest",
+    location: "Walnut, CA",
+    date: "March 23, 2018",
+    time: "3:30PM",
+    tag: "Social Good",
+  },
+  {
+    title: "Protest",
+    location: "Walnut, CA",
+    date: "March 23, 2018",
+    time: "3:30PM",
+    tag: "Social Good",
+  },
+  {
+    title: "Protest",
+    location: "Walnut, CA",
+    date: "March 23, 2018",
+    time: "3:30PM",
+    tag: "Social Good",
+  },
+];
+const Repeater = () => {
+  return (
+    <ul class="right-side">
+      {a.map((event) => {
+        return <Event_Card event={event} />;
+      })}
+    </ul>
+  );
+};
+
+class Browse_Content extends Component {
+  render() {
+    return (
+      <div>
+        <h1 class="title">
+          Browse Events
+          <h2>5,230 Available Events</h2>
+        </h1>
+
+        <Filter />
+        <div>{Repeater()}</div>
+      </div>
+    );
+  }
+}
+
+export default Browse_Content;
