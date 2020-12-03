5f4ecfa8c6f7c0769d8cb1502fbb6b63d4640a0d
gautamvenkatesh <vegautam@gmail.com>
2020-12-03 01:22:54 -0800

    added a tag to evnt card

--- final-proj/src/components/Event_Card/Event_Card.js
+++ final-proj/src/components/Event_Card/Event_Card.js
@@ -3,35 +3,39 @@ import "./event_card.css";
 
 const Event_Card = ({ event }) => {
   return (
-    <div class="padding-between-cards">
-      <div class="image-spacing">
-        <img src={event.image} alt="event image" />
-      </div>
-      <div class="card-body">
-        <h5 class="title-card">{event.name}</h5>
-        <div class="date-event">
-          <div class="upcoming-past">UPCOMING</div>
-        </div>
-        <div class="date-event">
-          <div class="date">
-            Start: {new Date(event.startDate).toString().slice(4, 15)}
+    <div>
+      <a href = {`/event/${event._id}`}>
+        <div class="padding-between-cards">
+          <div class="image-spacing">
+            <img src={event.image} alt="event image" />
           </div>
-          <div class="date">
-            End: {new Date(event.endDate).toString().slice(4, 15)}
-          </div>
-        </div>
-        <div class="date-event">
-          <div>Location: {event.location}</div>
-        </div>
+          <div class="card-body">
+            <h5 class="title-card">{event.name}</h5>
+            <div class="date-event">
+              <div class="upcoming-past">UPCOMING</div>
+            </div>
+            <div class="date-event">
+              <div class="date">
+                Start: {new Date(event.startDate).toString().slice(4, 15)}
+              </div>
+              <div class="date">
+                End: {new Date(event.endDate).toString().slice(4, 15)}
+              </div>
+            </div>
+            <div class="date-event">
+              <div>Location: {event.location}</div>
+            </div>
 
-        <ul className="tags">
-          <li>
-            <a href="#" class="tag">
-              {event.tag}
-            </a>
-          </li>
-        </ul>
-      </div>
+            <ul className="tags">
+              <li>
+                <a href="#" class="tag">
+                  {event.tag}
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </a>
     </div>
   );
 };
