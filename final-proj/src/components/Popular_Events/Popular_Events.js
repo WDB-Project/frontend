2502afa92ba3414a7c309cf3bd426a32d57f2cb0
dvaish <dvaish.edu@gmail.com>
2020-12-04 00:46:52 -0800

    connected popular events to backend

--- final-proj/src/components/Popular_Events/Popular_Events.js
+++ final-proj/src/components/Popular_Events/Popular_Events.js
@@ -1,10 +1,70 @@
 import React, { Component } from "react";
 import { Card, ListGroup, Row, Col } from 'react-bootstrap';
 import "./PopularEvents.css"
+import axios from 'axios'
 
+const url = "http://ec2-3-86-143-220.compute-1.amazonaws.com:3000/events/get"
+
+const EventRow = ({ event }) => {
+  return (
+    <ListGroup.Item>
+      <Row noGutters>
+        <p style={ { marginBottom:0 } }>{event.name}</p>
+        <Col className="d-flex justify-content-end">
+          <p style={ { marginBottom:0 } }>{new Date(event.startDate).toString().slice(4, 15)}</p>
+        </Col>
+      </Row>
+    </ListGroup.Item>
+  )
+}
+
+function DisplayEvents(items) {
+  if (items == undefined) {
+    return null
+  }
+  return (
+    <ListGroup variant="flush">
+      {items.map((event) => {
+        return <EventRow event={event} />
+      })}
+    </ListGroup>
+  )
+}
 
 class PopEvents extends Component{
-  render(){
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoaded: false,
+      data: undefined
+    }
+    this.getEvents = this.getEvents.bind(this)
+  }
+
+  componentDidMount() {
+    this.getEvents();
+    console.log(this.state.data);
+  }
+
+  getEvents() {
+    axios.get(url)
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          isLoaded: true,
+          data: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          isLoaded: false,
+          error: error,
+        });
+      });
+  }
+
+  render() {
     const cardStyle = {
       padding: 30,
       width: 500,
@@ -18,54 +78,19 @@ class PopEvents extends Component{
     };
     const pstyle = {
       marginBottom: 0
-    };
-    return (
-      <Card style={cardStyle}>
-        <Card.Header className="MontserratFont" style={title}>Upcoming</Card.Header>
-        <ListGroup variant="flush">
-          <ListGroup.Item>
-            <Row noGutters>
-              <p style={pstyle}>blah blah blah</p>
-              <Col className="d-flex justify-content-end">
-                <p style={pstyle}>date</p>
-              </Col>
-            </Row>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Row noGutters>
-              <p style={pstyle}>blah blah blah</p>
-              <Col className="d-flex justify-content-end">
-                <p style={pstyle}>date</p>
-              </Col>
-            </Row>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Row noGutters>
-              <p style={pstyle}>blah blah blah</p>
-              <Col className="d-flex justify-content-end">
-                <p style={pstyle}>date</p>
-              </Col>
-            </Row>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Row noGutters>
-              <p style={pstyle}>blah blah blah</p>
-              <Col className="d-flex justify-content-end">
-                <p style={pstyle}>date</p>
-              </Col>
-            </Row>
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Row noGutters>
-              <p style={pstyle}>blah blah blah</p>
-              <Col className="d-flex justify-content-end">
-                <p style={pstyle}>date</p>
-              </Col>
-            </Row>
-          </ListGroup.Item>
-        </ListGroup>
-      </Card>
-    )
+    };  
+    if (!this.state.isLoaded) {
+      return (<div>Loading...</div>)
+    } else {
+      return (
+        <Card style={cardStyle}>
+          <Card.Header className="MontserratFont" style={title}>Upcoming</Card.Header>
+          <ListGroup variant="flush">
+            {DisplayEvents(this.state.data.upcoming)}
+          </ListGroup>
+        </Card>
+      )
+    }
   }
 }
 
