a8589c43e978eda118f492d36f638c9e32fe79e3
Isabel Zheng <isabelzheng@Isabels-MacBook-Pro.local>
2020-12-07 15:53:41 -0800

    fonts and blobs

--- final-proj/src/components/Create_Event_Page/Create_Event_Page.js
+++ final-proj/src/components/Create_Event_Page/Create_Event_Page.js
@@ -8,7 +8,22 @@ class CreateEventPage extends Component {
     return(
       <div className="site-background">
         <Header />
-        <div className="d-flex justify-content-center padding">
+        <div className="position">
+
+          <svg className="blob2" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#243C36" d="M32.2,-4C39.2,11.1,40.7,34.6,26.7,47.1C12.8,59.7,-16.6,61.5,-38.7,46.9C-60.9,32.3,-75.9,1.4,-68.2,-14.7C-60.6,-30.7,-30.3,-31.9,-8.8,-29.1C12.6,-26.2,25.2,-19.2,32.2,-4Z" transform="translate(100 100)" />
+          </svg>
+
+          <svg className="blob4" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#E0EEC6" d="M25.9,-16.8C30.2,4.8,28,20.2,13.1,35C-1.8,49.8,-29.4,64.1,-43.7,54.9C-58.1,45.7,-59.3,13,-49.8,-15.8C-40.3,-44.5,-20.2,-69.4,-4.7,-67.9C10.8,-66.4,21.6,-38.4,25.9,-16.8Z" transform="translate(100 100)" />
+          </svg>
+
+          <svg className="blob5" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#243C36" d="M49,-8.2C57.3,9.5,53.5,38.9,35.9,52.4C18.3,65.9,-13.1,63.5,-25.5,50.8C-37.9,38.1,-31.3,15,-23.9,-1.5C-16.5,-18,-8.2,-27.9,6.1,-29.9C20.4,-31.9,40.8,-25.9,49,-8.2Z" transform="translate(100 100)" />
+          </svg>
+        </div>
+        <div className="d-flex justify-content-center card-style">
+          {/**/}
           <CreateEventCard />
         </div>
       </div>
--- final-proj/src/components/Home_Page/Home_Page.js
+++ final-proj/src/components/Home_Page/Home_Page.js
@@ -13,16 +13,33 @@ class HomePage extends Component{
         <Header />
 
         <div className="d-flex flex-row justify-content-around align-items-center" style={{height: 600, backgroundColor: '#F1F7ED'}}>
-          <div className="d-flex flex-column">
+          <div>
             <h1 className="title-style">Up&Coming</h1>
             <h4 className="subtitle-style">
-              subtitle blah blah blah blah blah
-              blah blah blah
+              events made easy
             </h4>
           </div>
           <div>
             <img src={Humans1} className="humans-style"/>
           </div>
+        </div>
+
+        <div>
+          <svg className="home-blob1" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#243C36" d="M35.8,-26C44.4,-5.8,47.9,11.9,41.1,28.2C34.3,44.4,17.2,59.1,0,59.1C-17.1,59.1,-34.3,44.4,-43.4,26.8C-52.6,9.2,-53.7,-11.2,-45.2,-31.4C-36.6,-51.5,-18.3,-71.4,-2.3,-70C13.6,-68.7,27.2,-46.1,35.8,-26Z" transform="translate(100 100)" />
+          </svg>
+
+          <svg className="home-blob2" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#E0EEC6" d="M54.5,-38.9C65.3,-29.9,65.1,-7.5,56.7,6.3C48.3,20.2,31.6,25.5,17.1,30.6C2.6,35.8,-9.7,40.6,-19.7,37.2C-29.6,33.8,-37.4,22,-41.8,8C-46.3,-6.1,-47.5,-22.5,-40,-30.8C-32.5,-39.1,-16.2,-39.3,2.8,-41.6C21.8,-43.8,43.6,-48,54.5,-38.9Z" transform="translate(100 100)" />
+          </svg>
+
+          <svg className="home-blob3" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#243C36" d="M50.2,-43.2C56.1,-32.7,45.6,-12.3,36.7,0.9C27.7,14.1,20.1,20.1,12.3,22.9C4.4,25.7,-3.8,25.2,-20.7,24.3C-37.6,23.3,-63.3,21.8,-72.1,9.1C-80.9,-3.6,-72.7,-27.6,-57.9,-40.2C-43.1,-52.7,-21.5,-53.9,0.3,-54.2C22.2,-54.4,44.3,-53.7,50.2,-43.2Z" transform="translate(100 100)" />
+          </svg>
+
+          <svg className="home-blob4" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#E0EEC6" d="M58.9,-47C71.9,-30.7,74.9,-6.4,67.3,10.8C59.8,28,41.5,38.2,23.1,46.4C4.7,54.7,-13.9,61,-27.5,55.2C-41.1,49.4,-49.8,31.3,-52.4,13.7C-55,-3.9,-51.5,-21.1,-41.8,-36.7C-32.1,-52.4,-16,-66.4,3.5,-69.1C23,-71.9,45.9,-63.4,58.9,-47Z" transform="translate(100 100)" />
+          </svg>
         </div>
 
         <div className="d-flex flex-row justify-content-around align-items-center">
